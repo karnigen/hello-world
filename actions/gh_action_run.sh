3fbc61bf9e1af476e5002f11e0dcc5e1d96c5040
karnigen <karnigen@gmail.com>
2025-05-31 22:45:00 +0200

    Automated Build

--- actions/gh_action_run.sh
+++ actions/gh_action_run.sh
@@ -15,6 +15,7 @@ BR=$(git rev-parse --abbrev-ref HEAD)
 #gh workflow run $WF -r kgn/pyproject-toml-investigation -f build_type=$build_type -f break_on=$break_on
 
 git commit -a -m "Automated Build"
+echo $?
 git push
 
 gh workflow run $WF -r $BR 
